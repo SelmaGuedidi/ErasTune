32ea5c64384567053d484179e434c12a19f0fde7
SelmaGuedidi <gueselma7@gmail.com>
2024-07-28 16:37:19 +0100

    Fix env variables

--- src/app/services/did-you-know-fact.service.ts
+++ src/app/services/did-you-know-fact.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, map } from 'rxjs';
-//import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
